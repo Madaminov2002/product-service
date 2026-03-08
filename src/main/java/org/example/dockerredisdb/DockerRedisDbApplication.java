c5d8f24b24ae381cb8ffb5db570766c1c841d275
Madaminov2002 <suhrobjonmadaminov37@gmail.com>
2026-03-08 11:56:10 +0500

    commit

--- src/main/java/org/example/dockerredisdb/DockerRedisDbApplication.java
+++ src/main/java/org/example/dockerredisdb/DockerRedisDbApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class DockerRedisDbApplication {
 
 //    @Bean
